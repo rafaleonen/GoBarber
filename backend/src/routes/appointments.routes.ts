22222af6a6044c1eb39fae61e825ea60edcbdbbd
rafahsclt <rafah.sclt@gmail.com>
2020-06-27 16:49:24 -0300

    database e funções

--- backend/src/routes/appointments.routes.ts
+++ backend/src/routes/appointments.routes.ts
@@ -1,30 +1,30 @@
 import { Router } from 'express';
+import { getCustomRepository } from 'typeorm'
 import { parseISO } from 'date-fns';
+
 import AppointmenstRepository from '../repositories/AppointmentsRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentRouter = Router();
-const appointmentsRepository = new AppointmenstRepository();
 
-appointmentRouter.get('/', (request, response) => {
-    const appointments = appointmentsRepository.all();
+appointmentRouter.get('/', async (request, response) => {
+    const appointmentsRepository = getCustomRepository(AppointmenstRepository)
+    const appointments = await appointmentsRepository.find();
 
     return response.json(appointments);
 });
 
-appointmentRouter.post('/', (request, response) => {
+appointmentRouter.post('/', async (request, response) => {
     try {
-        const { provider, date } = request.body;
+        const { provider_id , date } = request.body;
 
         const parsedDate = parseISO(date);
 
-        const createAppointment = new CreateAppointmentService(
-            appointmentsRepository,
-        );
+        const createAppointment = new CreateAppointmentService();
 
-        const appointment = createAppointment.execute({
+        const appointment = await createAppointment.execute({
             date: parsedDate,
-            provider,
+            provider_id,
         });
 
         return response.json(appointment);
