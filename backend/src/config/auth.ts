e50ffe5b1149a4c8a2b96d58e9ac8a04fb698fb9
rafahsclt <rafah.sclt@gmail.com>
2020-06-28 18:45:15 -0300

    database e serviços atualizados

---
+++ backend/src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+    jwt: {
+        secret: '1b92b18300b6e0c7cf9a3d81b052411e',
+        expiresIn: '1d'
+    }
+}
--- backend/src/routes/appointments.routes.ts
+++ backend/src/routes/appointments.routes.ts
@@ -5,9 +5,15 @@ import { parseISO } from 'date-fns';
 import AppointmenstRepository from '../repositories/AppointmentsRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
 
+import ensureAuthenticated from '../middlewares/ensureAuthenticated'
+
 const appointmentRouter = Router();
 
+appointmentRouter.use(ensureAuthenticated)
+
 appointmentRouter.get('/', async (request, response) => {
+    console.log(request.user)
+
     const appointmentsRepository = getCustomRepository(AppointmenstRepository)
     const appointments = await appointmentsRepository.find();
 
@@ -15,22 +21,18 @@ appointmentRouter.get('/', async (request, response) => {
 });
 
 appointmentRouter.post('/', async (request, response) => {
-    try {
-        const { provider_id , date } = request.body;
+    const { provider_id, date } = request.body;
 
-        const parsedDate = parseISO(date);
+    const parsedDate = parseISO(date);
 
-        const createAppointment = new CreateAppointmentService();
+    const createAppointment = new CreateAppointmentService();
 
-        const appointment = await createAppointment.execute({
-            date: parsedDate,
-            provider_id,
-        });
+    const appointment = await createAppointment.execute({
+        date: parsedDate,
+        provider_id,
+    });
 
-        return response.json(appointment);
-    } catch (err) {
-        return response.status(400).json({ error: err.message });
-    }
+    return response.json(appointment);
 });
 
 export default appointmentRouter;
