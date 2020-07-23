bda71c7beedea3a5b70d180171c301b43cbd7eb6
rafahsclt <rafah.sclt@gmail.com>
2020-07-23 13:35:11 -0300

    Alteração na estrutura (DDD) e adição de controllers

---
+++ backend/src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -0,0 +1,7 @@
+import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment'
+import ICreateAppointmentDTO from '../infra/dtos/ICreateAppointmentDTO'
+
+export default interface IApointmentsRepository {
+    create(data: ICreateAppointmentDTO): Promise<Appointment>
+    findByDate(date: Date): Promise<Appointment | undefined>
+}
--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -4,11 +4,12 @@ import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors'
 import cors from 'cors'
 
-import routes from './routes';
-import uploadConfig from './config/upload'
-import AppError from './errors/AppError'
+import routes from '@shared/infra/http/routes';
+import uploadConfig from '@config/upload'
+import AppError from '@shared/errors/AppError'
 
-import './database'
+import '@shared/infra/typeorm'
+import '@shared/container'
 
 const app = express();
 app.use(cors())
