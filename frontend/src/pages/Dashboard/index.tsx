d6fb044ed03bdb4527bfedf467bd50ed1646e847
Rafael Leonen <rafah.sclt@gmail.com>
2020-08-27 16:31:06 -0300

    Forgot e Reset password, implementaçao com a api

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
+import { Container } from './styles'
+
 const Dashboard: React.FC = () => {
   return (
-    <h1>Dashboard</h1>
+    <Container>
+      
+    </Container>
   )
 }
 
