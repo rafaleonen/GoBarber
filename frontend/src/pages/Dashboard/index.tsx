4c21c995b7959da00342314f4d655cab8abe305e
Rafael Leonen <rafah.sclt@gmail.com>
2020-08-28 13:08:11 -0300

    Adiçao de agendamentos no dashboard

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import { FiPower } from 'react-icons/fi'
+import { FiPower, FiClock } from 'react-icons/fi'
 
-import { Container, Header, HeaderContent, Profile } from './styles'
+import { Container, Header, HeaderContent, Profile, Content, Schedule, Calendar, NextAppointment, Section, Appointment } from './styles'
 import { useAuth } from '../../hooks/auth'
 import logoImg from '../../assets/logo.svg'
 
@@ -31,6 +31,61 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+      <Content>
+        <Schedule>
+          <h1>Horários Agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir :</strong>
+            <div>
+              <img src="https://avatars2.githubusercontent.com/u/60005589?s=460&u=550827fd57a4fa09357d90a1f9c956876e33677b&v=4" alt="Rafael Leonen"/>
+              <strong>Rafael Leonen</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+
+          <Section>
+            <strong>Manhã</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img src="https://avatars2.githubusercontent.com/u/60005589?s=460&u=550827fd57a4fa09357d90a1f9c956876e33677b&v=4" alt="Rafael Leonen"/>
+                <strong>Rafael Leonen</strong>
+              </div>
+            </Appointment>
+          </Section>
+
+          <Section>
+            <strong>Tarde</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img src="https://avatars2.githubusercontent.com/u/60005589?s=460&u=550827fd57a4fa09357d90a1f9c956876e33677b&v=4" alt="Rafael Leonen"/>
+                <strong>Rafael Leonen</strong>
+              </div>
+            </Appointment>
+          </Section>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   )
 }
